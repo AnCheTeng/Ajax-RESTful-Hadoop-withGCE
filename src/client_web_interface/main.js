a4e3a60d230bb646d8fa94064274755548ad712c
An-Che Teng <angeldan0627@gmail.com>
2015-11-16 15:04:18 +0800

    Add result button function

--- src/client_web_interface/main.js
+++ src/client_web_interface/main.js
@@ -2,10 +2,17 @@ $(document).ready(function(){
   $.getJSON('/list', function(result){
     $.each(result, function(index, task){
       var task_row = $('<tr></tr>');
-      task_row.append('<td>'+task.file+'</td>');
+      var filename = task.file;
+      task_row.append('<td data-filename='+filename+'>'+filename+'</td>');
       task_row.append('<td><button class="btn btn-info">'+task.state+'</button></td>');
       task_row.append('<td><button class="btn btn-warning">Edit</button></td>');
       $('tbody').append(task_row);
     })
+  });
+  $('button').on('click', function(){
+    var fn = $(this).parent().find("td").first().data('filename');
+    $.getJSON('/task/'+filename, function(task_info){
+      $('.hadoop-task-result').text(task_info.result);
+    });
   });
 })
